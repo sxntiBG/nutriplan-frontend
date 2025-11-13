6cdb6b625fe41bb94ebdb92af329c203c3d185b9
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-11-13 10:42:36 -0500

    feat(navbar): ajustar la navbar para que tenga el diseño con nutriplan

---
+++ src/app/shared/components/progress-bar-component/progress-bar-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar';
+
+@Component({
+  selector: 'app-progress-bar-component',
+  imports: [NavbarComponent],
+  templateUrl: './progress-bar-component.html',
+  styleUrl: './progress-bar-component.css',
+})
+export class ProgressBarComponent {
+
+}
