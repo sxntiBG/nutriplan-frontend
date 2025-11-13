22cdb59ab646117094a8406f0bb387e701b71c81
sxntiBG <zapataospinasantiago8@gmail.com>
2025-11-12 23:02:37 -0500

    feat: agregar vista register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { IndexComponent } from './pages/index/index';
-import { LoginComponent } from './pages/login/login';
+import { LoginComponent } from './pages/login/login'
+import { Register } from './pages/register/register';
 
 export const routes: Routes = [
   { path: '', component: IndexComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: Register },
   { path: '**', redirectTo: '' }
 ];
