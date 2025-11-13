2fd4749d2d7dee804c3f94cd80b7d12af31b9833
sxntiBG <zapataospinasantiago8@gmail.com>
2025-11-12 20:09:26 -0500

    feat: se agrega configuración de Tailwind y se crea la vista principal (index)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('nutriplan-frontend');
+  title = 'nutriplan-frontend';
 }
