2f16309f6a4b078bc1597dd15c9a82610d23fd9e
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-11-13 11:41:02 -0500

    fix: solucion de la integracion de la navbar con otros componentes

--- src/app/shared/components/progress-bar-component/progress-bar-component.ts
+++ src/app/shared/components/progress-bar-component/progress-bar-component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../navbar/navbar';
+
 
 @Component({
   selector: 'app-progress-bar-component',
-  imports: [NavbarComponent],
+   standalone: true, 
+  imports: [CommonModule, NavbarComponent],
   templateUrl: './progress-bar-component.html',
-  styleUrl: './progress-bar-component.css',
+  
 })
 export class ProgressBarComponent {
+  @Input() currentStep: number = 1;
+  @Input() totalSteps: number = 5;
 
+  get steps(): number[] {
+    return Array.from({ length: this.totalSteps }, (_, i) => i + 1);
+  }
 }
