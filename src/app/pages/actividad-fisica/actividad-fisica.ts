07f3efe6a87e800f9c0f95fa98587e7e45777701
Estefany Monsalve <monsalvepinoe@gmail.com>
2025-11-18 22:54:29 -0500

    feat(actividad-fisica.service): crear el servicio para obtener los datos de actividad fisica

---
+++ src/app/pages/actividad-fisica/actividad-fisica.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actividad-fisica',
+  imports: [],
+  templateUrl: './actividad-fisica.html',
+  styleUrl: './actividad-fisica.css',
+})
+export class ActividadFisica {
+
+}
